48c33f46f3c2cd1455db01d5d63b09e8e0a92710
Cesar Mayta <cesarmayta@gmail.com>
2021-10-14 20:59:13 -0500

    dia1

--- semana02/dia3/restaurant/base/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Plato,Categoria
-
-admin.site.register(Plato)
-admin.site.register(Categoria)
--- semana02/dia3/restaurant/comandas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComandasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'comandas'
